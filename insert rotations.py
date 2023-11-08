953efb730738aac03a7ff496a2e7558ab45b8d60
JustinStrik <strikowski.justin@gmail.com>
2023-11-08 12:49:08 -0500

    file for insert rotations

---
+++ insert rotations.py
@@ -0,0 +1,16 @@
+# LLb, RRb, LRb, RLb
+def rotate(node):
+
+# declare functions for all 4 rotations
+def LLb():
+    pass
+
+def RRb():
+    pass
+
+def LRb():
+    pass
+
+def RLb():
+    pass
+    
