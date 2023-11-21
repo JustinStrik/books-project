01887becc9fba17052cdb55618819c7e2811c653
JustinStrik <strikowski.justin@gmail.com>
2023-11-21 00:50:50 -0500

    fixed for starting at 0

--- reservationheap.py
+++ reservationheap.py
@@ -10,15 +10,12 @@ class ReservationNode:
     def add_child(self, node):
         self.children.append(node)
 
+# starts at 0, so left child is 1, right child is 2, etc
 def move_left(pos):
-    if (pos == 0):
-        return 1
-    return pos * 2
+    return pos * 2 + 1
 
 def move_right(pos):
-    if (pos == 0):
-        return 2
-    return pos * 2 + 1
+    return pos * 2 + 2
     
 class ReservationHeap:
     heap = []
@@ -28,44 +25,22 @@ class ReservationHeap:
 
     # takes in position in heap array
     def get_parent(self, pos):
-        if (pos == 0):
-            return None
-        elif (pos == 2):
-            return self.heap[0]
-        
-        return self.heap[pos // 2] # double slash is integer division, so floor
+        return self.heap[(pos - 1) // 2] # double slash is integer division, so floor
     
     def get_left(self, pos):
-        if (pos * 2 >= len(self.heap)):
-            return None
-        
-        elif (pos == 0):
-            if (len(self.heap) == 1):
-                return None
-            return self.heap[1]
-        
-        return self.heap[pos * 2]
-    
-    def get_right(self, pos):
         if (pos * 2 + 1 >= len(self.heap)):
             return None
         
-        elif (pos == 0):
-            if (len(self.heap) < 3):
-                return None
-            return self.heap[2]
-        
         return self.heap[pos * 2 + 1]
     
+    def get_right(self, pos):
+        if (pos * 2 + 2 >= len(self.heap)):
+            return None
+        
+        return self.heap[pos * 2 + 2]
+    
+    # swaps two nodes in the heap for heapify
     def swap(self, pos1, pos2):
-        # if 0,0, swap 0 and 1
-        # if 0,1, swap 0 and 2 since 0 multiplied by 2 is 0
-        if (pos1 == 0):
-            if (pos2 == 1):
-                pos2 = 2
-            else:
-                pos2 = 1
-
         self.heap[pos1], self.heap[pos2] = self.heap[pos2], self.heap[pos1]
 
     def insert(self, reservation_node):
@@ -77,14 +52,14 @@ class ReservationHeap:
             # if equal, check to see if the time is less
             if (reservation_node.patron_priority == self.get_parent(pos).patron_priority):
                 if (reservation_node.time < self.get_parent(pos).time):
-                    self.swap(pos, pos // 2)
-                    pos = pos // 2
+                    self.swap(pos, (pos - 1) // 2)
+                    pos = (pos - 1) // 2
                     continue
                 else:
                     break
             # not equal, just less: swap
-            self.swap(pos, pos // 2)
-            pos = pos // 2
+            self.swap(pos, (pos - 1) // 2)
+            pos = (pos - 1) // 2
 
     def remove(self):
         if (len(self.heap) == 0):
@@ -104,12 +79,12 @@ class ReservationHeap:
                 if (self.get_left(pos) != None and self.get_right(pos) != None):
                     if (self.get_left(pos).patron_priority < self.get_right(pos).patron_priority):
                         if (self.heap[pos].patron_priority > self.get_left(pos).patron_priority):
-                            self.swap(pos, pos * 2)
+                            self.swap(pos, pos * 2 + 1)
                             pos = move_left(pos)
                             continue
                         elif (self.heap[pos].patron_priority == self.get_left(pos).patron_priority):
                             if (self.heap[pos].time > self.get_left(pos).time):
-                                self.swap(pos, pos * 2)
+                                self.swap(pos, pos * 2 + 1)
                                 pos = move_left(pos)
                                 continue
                         else:
@@ -117,12 +92,12 @@ class ReservationHeap:
                             break
                     else:
                         if (self.heap[pos].patron_priority > self.get_right(pos).patron_priority):
-                            self.swap(pos, pos * 2 + 1)
+                            self.swap(pos, pos * 2 + 2)
                             pos = move_right(pos)
                             continue
                         elif (self.heap[pos].patron_priority == self.get_right(pos).patron_priority):
                             if (self.heap[pos].time > self.get_right(pos).time):
-                                self.swap(pos, pos * 2 + 1)
+                                self.swap(pos, pos * 2 + 2)
                                 pos = move_right(pos)
                                 continue
                         else:
@@ -131,12 +106,12 @@ class ReservationHeap:
                 # its possible for left to not be null, but its not possible for right and NOT left to be null
                 elif (self.get_left(pos) != None): 
                     if (self.heap[pos].patron_priority > self.get_left(pos).patron_priority):
-                        self.swap(pos, pos * 2)
+                        self.swap(pos, pos * 2 + 1)
                         pos = move_left(pos)
                         continue
                     elif (self.heap[pos].patron_priority == self.get_left(pos).patron_priority):
                         if (self.heap[pos].time > self.get_left(pos).time):
-                            self.swap(pos, pos * 2)
+                            self.swap(pos, pos * 2 + 1)
                             pos = move_left(pos) # should always be the end, since if right is null, then this left was last
                             continue
                     else:
@@ -176,10 +151,7 @@ class ReservationHeap:
                 #     break
                 
 
-                if (pos == 0):
-                    pos += 1
-                else:
-                    pos = pos * 2
+                done_heapifying = True # if here, then did not break and is done heapifying
 
         return removed
 
